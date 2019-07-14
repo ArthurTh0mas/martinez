9ad78f49cfff7bb197af40d6acde231635e40cf8
Annie Lai <68039932+ng8eke@users.noreply.github.com>
2019-07-14 21:43:50 +0800

    Add db-stats command

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+use martinez::table_sizes;
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+#[structopt(
+    name = "Martinez",
+    about = "Ethereum client based on turbo-geth architecture"
+)]
+pub enum Opt {
+    /// Print database statistics
+    DbStats {
+        /// Chain data path
+        #[structopt(parse(from_os_str))]
+        chaindata: PathBuf,
+    },
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let opt = Opt::from_args();
+
+    match opt {
+        Opt::DbStats { chaindata } => {
+            let env = mdbx::Environment::new().open(&chaindata)?;
+            for (table, size) in table_sizes(&env.begin_ro_txn()?)? {
+                println!("{} - {}", table, size);
+            }
+        }
+    }
+
+    Ok(())
+}
