3d25b939075d77eca669ca3ebe7a5b2ed2f1fae2
Annie Lai <68039932+ng8eke@users.noreply.github.com>
2019-11-12 01:28:53 +0800

    Extract-Transform–Load Framework for Martinez (ETL) Refresh2020

--- src/etl/collector.rs
+++ src/etl/collector.rs
@@ -1,44 +1,19 @@
 use super::data_provider::*;
-use crate::kv::{tables::ErasedTable, traits::*};
-use derive_more::*;
-use std::{
-    cmp::Reverse,
-    collections::BinaryHeap,
-    ops::{Generator, GeneratorState},
-    pin::Pin,
-};
-use tempfile::TempDir;
+use crate::{kv::Table, MutableCursor};
+use std::{cmp::Reverse, collections::BinaryHeap};
 
-pub struct Collector<'tmp, Key, Value>
-where
-    Key: TableEncode,
-    Value: TableEncode,
-    <Key as TableEncode>::Encoded: Ord,
-    <Value as TableEncode>::Encoded: Ord,
-    Vec<u8>: From<<Key as TableEncode>::Encoded>,
-    Vec<u8>: From<<Value as TableEncode>::Encoded>,
-{
-    tempdir: &'tmp TempDir,
+pub struct Collector {
     buffer_size: usize,
     data_providers: Vec<DataProvider>,
     buffer_capacity: usize,
-    buffer: Vec<Entry<<Key as TableEncode>::Encoded, <Value as TableEncode>::Encoded>>,
+    buffer: Vec<Entry>,
 }
 
 pub const OPTIMAL_BUFFER_CAPACITY: usize = 512000000; // 512 Megabytes
 
-impl<'tmp, Key, Value> Collector<'tmp, Key, Value>
-where
-    Key: TableEncode,
-    Value: TableEncode,
-    <Key as TableEncode>::Encoded: Ord,
-    <Value as TableEncode>::Encoded: Ord,
-    Vec<u8>: From<<Key as TableEncode>::Encoded>,
-    Vec<u8>: From<<Value as TableEncode>::Encoded>,
-{
-    pub fn new(tempdir: &'tmp TempDir, buffer_capacity: usize) -> Self {
-        Self {
-            tempdir,
+impl Collector {
+    pub fn new(buffer_capacity: usize) -> Collector {
+        Collector {
             buffer_size: 0,
             buffer_capacity,
             data_providers: Vec::new(),
@@ -46,140 +21,83 @@ where
         }
     }
 
-    fn flush(&mut self) {
-        self.buffer_size = 0;
-        self.buffer.sort_unstable();
-        let mut buf = Vec::with_capacity(self.buffer.len());
-        std::mem::swap(&mut buf, &mut self.buffer);
-        self.data_providers
-            .push(DataProvider::new(self.tempdir.path(), buf).unwrap());
-    }
-
-    pub fn push(&mut self, key: Key, value: Value) {
-        let key = key.encode();
-        let value = value.encode();
-        self.buffer_size += key.as_ref().len() + value.as_ref().len();
-        self.buffer.push(Entry { key, value });
+    pub fn collect(&mut self, entry: Entry) {
+        self.buffer_size += entry.key.len() + entry.value.len();
+        self.buffer.push(entry);
         if self.buffer_size > self.buffer_capacity {
-            self.flush();
+            self.buffer_size = 0;
+            self.buffer.sort_unstable();
+            let current_id = self.data_providers.len();
+            self.data_providers
+                .push(DataProvider::new(self.buffer.clone(), current_id).unwrap());
+            self.buffer.clear();
         }
-    }
-
-    pub fn iter(&mut self) -> CollectorIter<'_> {
-        CollectorIter {
-            done: false,
-            inner: Box::pin(|| {
-                // If only one data provider is found, then we we can write directly from memory to db without reading any files
-                if self.data_providers.is_empty() {
-                    self.buffer.sort_unstable();
-                    for entry in self.buffer.drain(..) {
-                        yield Ok((entry.key.into(), entry.value.into()));
-                    }
-                    return Ok(());
-                }
-                // Flush buffer one more time
-                if self.buffer_size != 0 {
-                    self.flush();
-                }
-
-                let mut heap = BinaryHeap::new();
-
-                // Anchor each data provider in the heap
-                for (current_id, data_provider) in self.data_providers.iter_mut().enumerate() {
-                    if let Some((current_key, current_value)) = data_provider.to_next()? {
-                        heap.push(Reverse((
-                            Entry::new(current_key, current_value),
-                            current_id,
-                        )));
-                    }
-                }
-
-                // Take the lowest entry from all data providers in the heap.
-                while let Some(Reverse((Entry { key, value }, id))) = heap.pop() {
-                    yield Ok((key, value));
-                    if let Some((next_key, next_value)) = self.data_providers[id].to_next()? {
-                        // Insert another from the same data provider unless it's exhausted.
-                        heap.push(Reverse((Entry::new(next_key, next_value), id)));
-                    }
-                }
-                Ok(())
-            }),
-        }
-    }
-}
-
-pub struct CollectorIter<'a> {
-    done: bool,
-    inner: Pin<
-        Box<
-            dyn Generator<Yield = anyhow::Result<(Vec<u8>, Vec<u8>)>, Return = anyhow::Result<()>>
-                + Send
-                + 'a,
-        >,
-    >,
-}
-
-impl<'a> Iterator for CollectorIter<'a> {
-    type Item = anyhow::Result<(Vec<u8>, Vec<u8>)>;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        if self.done {
-            return None;
-        }
-
-        match Pin::new(&mut self.inner).resume(()) {
-            GeneratorState::Yielded(res) => Some(res),
-            GeneratorState::Complete(res) => {
-                self.done = true;
-                match res {
-                    Ok(()) => None,
-                    Err(e) => Some(Err(e)),
-                }
-            }
-        }
-    }
-}
-
-#[derive(Deref, DerefMut, From)]
-pub struct TableCollector<'tmp, T>(Collector<'tmp, T::Key, T::Value>)
-where
-    T: Table,
-    T::Key: TableEncode,
-    T::Value: TableEncode,
-    <<T as Table>::Key as TableEncode>::Encoded: Ord,
-    <<T as Table>::Value as TableEncode>::Encoded: Ord,
-    Vec<u8>: From<<<T as Table>::Key as TableEncode>::Encoded>,
-    Vec<u8>: From<<<T as Table>::Value as TableEncode>::Encoded>;
-
-impl<'tmp, T> TableCollector<'tmp, T>
-where
-    T: Table,
-    T::Key: TableEncode,
-    T::Value: TableEncode,
-    <<T as Table>::Key as TableEncode>::Encoded: Ord,
-    <<T as Table>::Value as TableEncode>::Encoded: Ord,
-    Vec<u8>: From<<<T as Table>::Key as TableEncode>::Encoded>,
-    Vec<u8>: From<<<T as Table>::Value as TableEncode>::Encoded>,
-{
-    pub fn new(tempdir: &'tmp TempDir, buffer_capacity: usize) -> Self {
-        Self(Collector::new(tempdir, buffer_capacity))
-    }
-
-    pub fn into_inner(self) -> Collector<'tmp, T::Key, T::Value> {
-        self.0
     }
 
     #[allow(clippy::type_complexity)]
-    pub async fn load<'tx, C>(&mut self, cursor: &mut C) -> anyhow::Result<()>
+    pub async fn load<'tx, T, C>(
+        &mut self,
+        cursor: &mut C,
+        load_function: Option<fn(&mut C, Vec<u8>, Vec<u8>)>,
+    ) -> anyhow::Result<()>
     where
-        C: MutableCursor<'tx, ErasedTable<T>>,
+        T: Table,
+        C: MutableCursor<'tx, T>,
     {
-        for res in self.iter() {
-            let (k, v) = res?;
-
-            cursor.put(k, v).await?;
+        // If only one data provider is found, then we we can write directly from memory to db without reading any files
+        if self.data_providers.is_empty() {
+            self.buffer.sort_unstable();
+            for entry in &self.buffer {
+                if let Some(f) = &load_function {
+                    (f)(cursor, entry.key.to_vec(), entry.value.to_vec());
+                } else {
+                    cursor
+                        .put(entry.key.as_slice(), entry.value.as_slice())
+                        .await?;
+                }
+            }
+            self.buffer.clear();
+            return Ok(());
+        }
+        // Flush buffer one more time
+        if self.buffer_size != 0 {
+            self.buffer.sort_unstable();
+            let current_id = self.data_providers.len();
+            self.data_providers
+                .push(DataProvider::new(self.buffer.clone(), current_id).unwrap());
+            self.buffer.clear();
         }
 
+        let mut heap = BinaryHeap::new();
+
+        for (current_id, data_provider) in self.data_providers.iter_mut().enumerate() {
+            let (current_key, current_value) = data_provider.to_next()?;
+
+            heap.push(Reverse(Entry {
+                key: current_key,
+                value: current_value,
+                id: current_id,
+            }));
+        }
+
+        while let Some(e) = heap.pop() {
+            let entry = e.0;
+            if let Some(f) = &load_function {
+                (f)(cursor, entry.key.to_vec(), entry.value.to_vec());
+            } else {
+                cursor
+                    .put(entry.key.as_slice(), entry.value.as_slice())
+                    .await?;
+            }
+            let (next_key, next_value) = self.data_providers[entry.id].to_next()?;
+            if !next_key.is_empty() {
+                heap.push(Reverse(Entry {
+                    key: next_key,
+                    value: next_value,
+                    id: entry.id,
+                }));
+            }
+        }
         Ok(())
     }
 }
@@ -187,69 +105,83 @@ where
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::{
-        kv::{new_mem_database, tables},
-        models::BlockNumber,
+    use crate::kv::{
+        new_mem_database, tables,
+        traits::{MutableKV, MutableTransaction, Transaction},
     };
+    use rand::{distributions::Uniform, Rng}; // 0.6.5
+    use std::u64;
 
     #[tokio::test]
     async fn collect_all_at_once() {
+        let mut rng = rand::thread_rng();
+        let range = Uniform::new(0, u64::MAX);
         // generate random entries
-        let mut entries: Vec<(_, _)> = (0..10000)
-            .map(|_| (rand::random(), BlockNumber(rand::random())))
+        let mut entries: Vec<Entry> = (0..10000)
+            .map(|_| Entry {
+                key: rng.sample(&range).to_be_bytes().to_vec(),
+                value: rng.sample(&range).to_be_bytes().to_vec(),
+                id: 0,
+            })
             .collect();
         let db = new_mem_database().unwrap();
         let tx = db.begin_mutable().await.unwrap();
-        let temp_dir = tempfile::tempdir().unwrap();
-        let mut collector = TableCollector::new(&temp_dir, OPTIMAL_BUFFER_CAPACITY);
+        let mut collector = Collector::new(OPTIMAL_BUFFER_CAPACITY);
 
-        for (key, value) in entries.clone() {
-            collector.push(key, value);
+        for entry in entries.clone() {
+            collector.collect(entry);
         }
         // Any cursor is fine
-        let mut cursor = tx
-            .mutable_cursor(tables::HeaderNumber.erased())
-            .await
-            .unwrap();
-        collector.load(&mut cursor).await.unwrap();
+        let mut cursor = tx.mutable_cursor(&tables::HeaderNumber).await.unwrap();
+        collector.load(&mut cursor, None).await.unwrap();
 
         // We sort the entries and compare them to what is in db
         entries.sort_unstable();
 
-        for (key, value) in entries {
-            if let Some(expected_value) = tx.get(tables::HeaderNumber, key).await.unwrap() {
-                assert_eq!(value, expected_value);
+        for entry in entries {
+            if let Some(expected_value) = tx
+                .get(&tables::HeaderNumber, entry.key.as_slice())
+                .await
+                .unwrap()
+            {
+                assert_eq!(entry.value, expected_value);
             }
         }
     }
 
     #[tokio::test]
     async fn collect_chunks() {
+        let mut rng = rand::thread_rng();
+        let range = Uniform::new(0, u64::MAX);
         // generate random entries
-        let mut entries: Vec<(_, _)> = (0..5000)
-            .map(|_| (rand::random(), BlockNumber(rand::random())))
+        let mut entries: Vec<Entry> = (0..10000)
+            .map(|_| Entry {
+                key: rng.sample(&range).to_be_bytes().to_vec(),
+                value: rng.sample(&range).to_be_bytes().to_vec(),
+                id: 0,
+            })
             .collect();
         let db = new_mem_database().unwrap();
         let tx = db.begin_mutable().await.unwrap();
-        let temp_dir = tempfile::tempdir().unwrap();
-        let mut collector = TableCollector::new(&temp_dir, 1000);
+        let mut collector = Collector::new(1000);
 
-        for (key, value) in entries.clone() {
-            collector.push(key, value);
+        for entry in entries.clone() {
+            collector.collect(entry);
         }
         // Any cursor is fine
-        let mut cursor = tx
-            .mutable_cursor(tables::HeaderNumber.erased())
-            .await
-            .unwrap();
-        collector.load(&mut cursor).await.unwrap();
+        let mut cursor = tx.mutable_cursor(&tables::HeaderNumber).await.unwrap();
+        collector.load(&mut cursor, None).await.unwrap();
 
         // We sort the entries and compare them to what is in db
         entries.sort_unstable();
 
-        for (key, value) in entries {
-            if let Some(expected_value) = tx.get(tables::HeaderNumber, key).await.unwrap() {
-                assert_eq!(value, expected_value);
+        for entry in entries {
+            if let Some(expected_value) = tx
+                .get(&tables::HeaderNumber, entry.key.as_slice())
+                .await
+                .unwrap()
+            {
+                assert_eq!(entry.value, expected_value);
             }
         }
     }
--- src/etl/data_provider.rs
+++ src/etl/data_provider.rs
@@ -2,78 +2,74 @@ use anyhow;
 use std::{
     cmp::Ord,
     fs::File,
-    io::{prelude::*, BufReader, BufWriter, SeekFrom},
-    path::Path,
+    io::{prelude::*, SeekFrom},
 };
-use tempfile::tempfile_in;
+use tempfile::tempfile;
 
-#[derive(Eq, Clone, PartialEq, PartialOrd, Ord)]
-pub struct Entry<Key, Value> {
-    pub key: Key,
-    pub value: Value,
+pub trait Provider {
+    fn new(buffer: Vec<Entry>, id: usize) -> anyhow::Result<Self, std::io::Error>
+    where
+        Self: Sized;
+    fn to_next(&mut self) -> anyhow::Result<(Vec<u8>, Vec<u8>)>;
 }
 
-impl<Key, Value> Entry<Key, Value> {
-    pub fn new(key: Key, value: Value) -> Self {
-        Self { key, value }
-    }
+#[derive(Eq, Clone, PartialEq, PartialOrd, Ord)]
+pub struct Entry {
+    pub key: Vec<u8>,
+    pub value: Vec<u8>,
+    pub id: usize,
 }
 
 pub struct DataProvider {
-    file: BufReader<File>,
-    len: usize,
+    pub file: File,
+    pub id: usize,
 }
 
-impl DataProvider {
-    pub fn new<Key, Value>(
-        dir: &Path,
-        buffer: Vec<Entry<Key, Value>>,
-    ) -> anyhow::Result<DataProvider, std::io::Error>
+impl Provider for DataProvider {
+    fn new(buffer: Vec<Entry>, id: usize) -> anyhow::Result<DataProvider, std::io::Error>
     where
         Self: Sized,
-        Key: AsRef<[u8]>,
-        Value: AsRef<[u8]>,
     {
-        let file = tempfile_in(dir)?;
-        let mut w = BufWriter::new(file);
-        for entry in &buffer {
-            let k = entry.key.as_ref();
-            let v = entry.value.as_ref();
-
-            w.write_all(&k.len().to_be_bytes())?;
-            w.write_all(&v.len().to_be_bytes())?;
-            w.write_all(k)?;
-            w.write_all(v)?;
+        let mut file = tempfile()?;
+        for entry in buffer {
+            file.write(&entry.key.len().to_be_bytes())?;
+            file.write(&entry.value.len().to_be_bytes())?;
+            file.write(&entry.key)?;
+            file.write(&entry.value)?;
         }
-
-        let mut file = BufReader::new(w.into_inner()?);
+        // Reset position at 0 byte
         file.seek(SeekFrom::Start(0))?;
-        let len = buffer.len();
-        Ok(DataProvider { file, len })
+        Ok(DataProvider { file, id })
     }
 
     #[allow(clippy::wrong_self_convention)]
-    pub fn to_next(&mut self) -> anyhow::Result<Option<(Vec<u8>, Vec<u8>)>> {
-        if self.len == 0 {
-            return Ok(None);
-        }
-
+    fn to_next(&mut self) -> anyhow::Result<(Vec<u8>, Vec<u8>)> {
         let mut buffer_key_length = [0; 8];
         let mut buffer_value_length = [0; 8];
 
-        self.file.read_exact(&mut buffer_key_length)?;
-        self.file.read_exact(&mut buffer_value_length)?;
+        // EOF reached
+        if self.file.read(&mut buffer_key_length[..])? == 0 {
+            return Ok((Vec::new(), Vec::new()));
+        }
+        // EOF reached
+        if self.file.read(&mut buffer_value_length[..])? == 0 {
+            return Ok((Vec::new(), Vec::new()));
+        }
 
         let key_length = usize::from_be_bytes(buffer_key_length);
         let value_length = usize::from_be_bytes(buffer_value_length);
         let mut key = vec![0; key_length];
         let mut value = vec![0; value_length];
 
-        self.file.read_exact(&mut key)?;
-        self.file.read_exact(&mut value)?;
+        // EOF reached
+        if self.file.read(&mut key[..])? == 0 {
+            return Ok((Vec::new(), Vec::new()));
+        }
 
-        self.len -= 1;
-
-        Ok(Some((key, value)))
+        // EOF reached
+        if self.file.read(&mut value[..])? == 0 {
+            return Ok((Vec::new(), Vec::new()));
+        }
+        Ok((key, value))
     }
 }
