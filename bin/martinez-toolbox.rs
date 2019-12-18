b39382648a9803a86518235d6b20db2869d83a75
Annie Lai <68039932+ng8eke@users.noreply.github.com>
2019-12-19 01:00:34 +0800

    toolbox: print all MDBX tables

--- bin/martinez-toolbox.rs
+++ bin/martinez-toolbox.rs
@@ -23,7 +23,7 @@ pub enum Opt {
 }
 
 async fn blockhashes(chaindata: PathBuf) -> anyhow::Result<()> {
-    let env = martinez::MdbxEnvironment::<mdbx::NoWriteMap>::open_ro(
+    let env = martinez::MdbxEnvironment::<mdbx::NoWriteMap>::open_rw(
         mdbx::Environment::new(),
         &chaindata,
         &martinez::kv::tables::TABLE_MAP,
@@ -38,7 +38,7 @@ async fn table_sizes(chaindata: PathBuf, csv: bool) -> anyhow::Result<()> {
     let env = martinez::MdbxEnvironment::<mdbx::NoWriteMap>::open_ro(
         mdbx::Environment::new(),
         &chaindata,
-        &martinez::kv::tables::TABLE_MAP,
+        2,
     )?;
     let mut sizes = mdbx_table_sizes(&env.begin_ro_txn()?)?
         .into_iter()
