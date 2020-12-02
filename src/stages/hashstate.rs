da57bdde9e3fb2d0e795e6ab8f95215d37b501c6
Annie Lai <68039932+ng8eke@users.noreply.github.com>
2020-12-03 02:35:44 +0800

    State::erase_storage

--- src/stages/hashstate.rs
+++ src/stages/hashstate.rs
@@ -32,7 +32,7 @@ where
         collector_account.collect(Entry::new(keccak256(address), account));
 
         i += 1;
-        if i % 500_000 == 0 {
+        if i % 5_000_000 == 0 {
             debug!("Converted {} entries", i);
         }
     }
@@ -60,7 +60,7 @@ where
         collector_storage.collect(Entry::new(keccak256(address), (keccak256(location), value)));
 
         i += 1;
-        if i % 500_000 == 0 {
+        if i % 5_000_000 == 0 {
             debug!("Converted {} entries", i);
         }
     }
@@ -138,7 +138,8 @@ pub struct HashState {
 impl HashState {
     pub fn new(clean_promotion_threshold: Option<u64>) -> Self {
         Self {
-            clean_promotion_threshold: clean_promotion_threshold.unwrap_or(1_000_000_000_000),
+            clean_promotion_threshold: clean_promotion_threshold
+                .unwrap_or(1_000_000_u64 * 1_000_000_u64),
         }
     }
 }
