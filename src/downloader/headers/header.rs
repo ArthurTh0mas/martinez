573832406dc69cdf618737a6b013200a0c4ad002
Annie Lai <68039932+ng8eke@users.noreply.github.com>
2021-01-02 09:56:56 +0800

    Downloader: refactor header. qtFramework

--- src/downloader/headers/header.rs
+++ src/downloader/headers/header.rs
@@ -1,12 +1,9 @@
-use crate::{crypto::keccak256, models};
-use bytes::Bytes;
+use crate::models;
 use ethereum_types::{H256, U256};
 
 #[derive(Clone, Debug)]
 pub struct BlockHeader {
     pub header: models::BlockHeader,
-    rlp_repr_cached: Option<Bytes>,
-    hash_cached: Option<H256>,
 }
 
 impl BlockHeader {
@@ -25,47 +22,13 @@ impl BlockHeader {
     pub fn timestamp(&self) -> u64 {
         self.header.timestamp
     }
-
-    fn rlp_repr_compute(&self) -> Bytes {
-        rlp::encode(&self.header).freeze()
-    }
-
-    pub fn rlp_repr_prepare(&mut self) {
-        self.rlp_repr_cached = Some(self.rlp_repr_compute());
-    }
-
-    pub fn rlp_repr(&self) -> Bytes {
-        self.rlp_repr_cached
-            .clone()
-            .unwrap_or_else(|| self.rlp_repr_compute())
-    }
-
-    fn hash_compute(rlp_repr: &Bytes) -> H256 {
-        keccak256(rlp_repr.as_ref())
-    }
-
-    pub fn hash_prepare(&mut self) {
-        if self.rlp_repr_cached.is_none() {
-            self.rlp_repr_prepare();
-        }
-        // Not calling self.rlp_repr(), because it causes an extra clone,
-        // but we just need a ref here.
-        let rlp_repr_cached = self.rlp_repr_cached.as_ref().unwrap();
-        self.hash_cached = Some(Self::hash_compute(rlp_repr_cached))
-    }
-
     pub fn hash(&self) -> H256 {
-        self.hash_cached
-            .unwrap_or_else(|| Self::hash_compute(&self.rlp_repr()))
+        self.header.hash()
     }
 }
 
 impl From<models::BlockHeader> for BlockHeader {
     fn from(header: models::BlockHeader) -> Self {
-        Self {
-            header,
-            rlp_repr_cached: None,
-            hash_cached: None,
-        }
+        Self { header }
     }
 }
