34b8ee1d92f0641e99099450ec4e13fa0476e949
Annie Lai <68039932+ng8eke@users.noreply.github.com>
2021-06-15 05:06:38 +0800

    Update deps

--- src/accessors/chain.rs
+++ src/accessors/chain.rs
@@ -206,8 +206,7 @@ pub mod block_body {
         number: impl Into<BlockNumber>,
     ) -> anyhow::Result<Option<(BlockBody, TxIndex)>> {
         if let Some(body) = super::storage_body::read(tx, hash, number).await? {
-            let transactions =
-                super::tx::read(tx, body.base_tx_id, body.tx_amount.try_into()?).await?;
+            let transactions = super::tx::read(tx, body.base_tx_id, body.tx_amount).await?;
 
             return Ok(Some((
                 BlockBody {
--- src/models/header.rs
+++ src/models/header.rs
@@ -2,11 +2,10 @@ use super::BlockNumber;
 use crate::crypto::*;
 use bytes::Bytes;
 use ethereum_types::*;
-use parity_scale_codec::*;
 use rlp::*;
 use serde::*;
 
-#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize, Encode, Decode)]
+#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
 /// Ethereum block header definition.
 pub struct BlockHeader {
     pub parent_hash: H256,
